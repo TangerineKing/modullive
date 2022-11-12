264b7fe5602ada51efc71d502517ba690b92e7e7
TangerineKing <118043611+TangerineKing@users.noreply.github.com>
2022-11-12 11:05:53 +0300

    Update ajax-form.php

--- ajax-form.php
+++ ajax-form.php
@@ -1,22 +1,18 @@
 <?php
-//Для начала проверим есть ли данные в полях name и email, что бы не слать совсем пустые формы :)
-//Если всё в порядке, то работаем дальше
+
 if (isset($_POST["name"]) && isset($_POST["mobile"]) ) { 
 
-//Принимаем данные POST-запроса и записываем значения в переменные
 
 $name = $_POST['name'];
 $mobile = $_POST['mobile']; 
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-//Теперь давайте настроим куда отправляем и откуда
 
-$my_email = 'supermsb@modullive.ru'; // Куда отправляем
-$sender_email = '<supermsb@modullive.ru>'; // От кого отправляем
+$my_email = 'private-information@oleg.com'; 
+$sender_email = '<again-private@oleg.com>'; 
 $title = "Ура,нам письмо пришло"; 
 
-//Сообщение, которое приходит на почту со всеми нужными нам данными:
 
 $mes = "
  Имя: $name\n
@@ -24,7 +20,6 @@ $mes = "
  Комментарий: $message\n
 ";
 
-//Всё, теперь можно отправлять письмо на почту
 
 $send = mail ($my_email,$title,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$sender_email");
 
